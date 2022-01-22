7628da1bbc2a7dc90b3f53e8df51cf3258cd0eb3
Juliano Cavalcante Teixeira da Silva <juliano.cavalcante98@gmail.com>
2022-01-22 12:46:15 -0300

    Improve movement smothness in the hour and minute pointers

--- scripts/index.js
+++ scripts/index.js
@@ -4,9 +4,9 @@ const minutesPointer = document.getElementById("minutes");
 const hoursPointer = document.getElementById("hours");
 const timezoneSelect = document.getElementById("timezoneSelect");
 
-const DEGREE_PER_SECOND = 360 / 60;
-const DEGREE_PER_MINUTE = 360 / 60;
-const DEGREE_PER_HOUR = 360 / 12;
+const DEGREES_PER_SECOND = 360 / 60;
+const DEGREES_PER_MINUTE = 360 / 60;
+const DEGREES_PER_HOUR = 360 / 12;
 
 let timeOffset = 0;
 let isOnline = true;
@@ -56,25 +56,28 @@ function sleep(ms) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
-function updateSecondsPointer(seconds) {
-  const degreesToRotate = seconds * DEGREE_PER_SECOND;
+function updateSecondsPointer(date) {
+  const degreesToRotate = date.getSeconds() * DEGREES_PER_SECOND;
   secondsPointer.style.transform = `rotate(${degreesToRotate}deg)`;
 }
 
-function updateMinutesPointer(minutes) {
-  const degreesToRotate = minutes * DEGREE_PER_MINUTE;
+function updateMinutesPointer(date) {
+  const degreesToRotate =
+    (date.getMinutes() + date.getSeconds() / 60) * DEGREES_PER_MINUTE;
   minutesPointer.style.transform = `rotate(${degreesToRotate}deg)`;
 }
 
-function updateHoursPointer(hours) {
-  const degreesToRotate = hours * DEGREE_PER_HOUR;
+function updateHoursPointer(date) {
+  const degreesToRotate =
+    (date.getHours() + date.getMinutes() / 60 + date.getSeconds() / 3600) *
+    DEGREES_PER_HOUR;
   hoursPointer.style.transform = `rotate(${degreesToRotate}deg)`;
 }
 
 function updateClock(date) {
-  updateSecondsPointer(date.getSeconds());
-  updateMinutesPointer(date.getMinutes());
-  updateHoursPointer(date.getHours());
+  updateSecondsPointer(date);
+  updateMinutesPointer(date);
+  updateHoursPointer(date);
 }
 
 async function mainLoop() {
